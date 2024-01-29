27e9f6ddb0a6d70e75f90bd9cb1ba90363794877
Denys C <d.chebotaev@protonmail.com>
2024-01-29 18:13:53 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,4 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,4 +24,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
   }
-});
